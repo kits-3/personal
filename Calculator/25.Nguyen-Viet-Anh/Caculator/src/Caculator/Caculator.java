a08e355c2f2cea98910c279dffc0dbe83455b5a5
vanhnguyen <0ozstarzo0@gmail.com>
2020-07-17 08:30:08 +0700

    new folder Calculator

---
+++ Calculator/25.Nguyen-Viet-Anh/Caculator/src/Caculator/Caculator.java
@@ -0,0 +1,45 @@
+package Caculator;
+
+import java.util.Scanner;
+
+public class Caculator {
+	public static void main(String[] args) {
+		float num1, num2;
+		String pt;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nhap a: ");
+		num1 = sc.nextFloat();
+		System.out.print("Nhap b: ");
+		num2 = sc.nextFloat();
+		System.out.print("Nhap phep tinh: ");
+		pt = sc.next();
+		
+		
+		
+		switch (pt) {
+			case "+":
+				System.out.println("KQ:  " + (num1 + num2));
+				break;
+			case "-":
+				System.out.println("KQ: " + (num1 - num2));
+				break;
+			case "*":
+				System.out.println("KQ: " + (num1 * num2));
+				break;
+			case "/":
+				if (num2 == 0) {
+					System.out.println("Not found !");
+					break;
+				}
+				else
+					System.out.println("KQ: " + (num1 / num2));
+					break;
+			default:
+				System.out.println("Not found !");
+				break;
+		}
+		
+		
+	}
+}
