6ce5525580a45a577bd90f82ec4e1c09cac575ac
Cong <congnguyen1496@gmail.com>
2020-07-20 16:35:49 +0700

    17

--- Vending machine -Inheritance, abstract, interface/17.NguyenThanhCong/maina.java
+++ Vending machine -Inheritance, abstract, interface/17.NguyenThanhCong/maina.java
@@ -13,13 +13,35 @@ public class maina {
 		String choose = sc.next();
 		if(choose.equals("food")){
 			vending_machine1 food = new vending_machine1();
+			food.list2.add(20);
+			food.list2.add(30);
+			food.list2.add(40);
+			food.list2.add(10);
+			
+			food.list3.add("food11");
+			food.list3.add("food2");
+			food.list3.add("food3");
+			food.list3.add("food4");
 			int money = 0;
+			food.vending_machine(money);
+			
 			food.vending_machine(money);
 		}
 		if(choose.equals("drink")){
-			vending_machine2 drink = new vending_machine2();
+			vending_machine2 food = new vending_machine2();
+			food.list2.add(20);
+			food.list2.add(30);
+			food.list2.add(40);
+			food.list2.add(10);
+			
+			food.list3.add("drink1");
+			food.list3.add("drink2");
+			food.list3.add("drink3");
+			food.list3.add("drink4");
 			int money = 0;
-			drink.vending_machine(money);
+			food.vending_machine(money);
+			
+			food.vending_machine(money);
 		}
 		
 		
