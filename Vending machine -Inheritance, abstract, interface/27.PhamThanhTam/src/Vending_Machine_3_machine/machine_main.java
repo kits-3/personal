1531aa772d9ae619b6b6283d5b4b355a120129c9
unknown <tamphamthanh1996@gmail.com>
2020-07-20 16:45:57 +0700

    update vending 3 machine

--- Vending machine -Inheritance, abstract, interface/27.PhamThanhTam/src/Vending_Machine_3_machine/machine_main.java
+++ Vending machine -Inheritance, abstract, interface/27.PhamThanhTam/src/Vending_Machine_3_machine/machine_main.java
@@ -6,17 +6,18 @@ public class machine_main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("order machine: 1 2 or 3");
+		System.out.println("order machine: 1-drink 2-food 3-computer");
+		System.out.println("choose: 1 2 or 3");
 		int key = scan.nextInt();
 		switch (key) {
 		case 1:
-			machine_1_drink drink = new machine_1_drink("coffee","milk", "juice", 100, 200, 300);
+			machine_drink_food__computer drink = new machine_drink_food__computer("coffee","milk", "juice", 100, 200, 300);
 			break;
 		case 2:
-			machine_2_food food = new machine_2_food("FOOD_1","FOOD_2", "FOOD_3", 1000, 2000, 3000);
+			machine_drink_food__computer food = new machine_drink_food__computer("FOOD_1","FOOD_2", "FOOD_3", 1000, 2000, 3000);
 		break;
 		case 3:
-			machine_3_computer computer = new machine_3_computer("COMPUTER_E","COMPUTER_D", "COMPUTER_F", 10, 20, 30);
+			machine_drink_food__computer computer = new machine_drink_food__computer("COMPUTER_E","COMPUTER_D", "COMPUTER_F", 10, 20, 30);
 		break;
 		default:
 			break;
