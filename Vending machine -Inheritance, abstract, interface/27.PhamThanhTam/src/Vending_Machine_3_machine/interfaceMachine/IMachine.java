a67b0f479fcc674142adea7063ffa8d471b7e795
unknown <tamphamthanh1996@gmail.com>
2020-07-20 17:57:31 +0700

    update vending 3 machine v2

--- Vending machine -Inheritance, abstract, interface/27.PhamThanhTam/src/Vending_Machine_3_machine/interfaceMachine/IMachine.java
+++ Vending machine -Inheritance, abstract, interface/27.PhamThanhTam/src/Vending_Machine_3_machine/interfaceMachine/IMachine.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public interface IMachine {
 	boolean test_Product(String selectProduct, String product);
 	boolean test_Product_Price(int productPrice, int money);
-	void information(int productPrice, String product, int money);
+	int information(int productPrice, String product, int money);
 	void order_machine(boolean checkMore, int money,String[] product,int[] productPrice,int count1,int count2 , int count3,HashMap<String,Integer> orderProduct );
 
 }
