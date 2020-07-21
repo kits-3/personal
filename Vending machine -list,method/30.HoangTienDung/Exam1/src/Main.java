b23dfa10e4563cdc6bb76774e376f48c317641df
unknown <dung23cm@gmail.com>
2020-07-21 14:00:25 +0700

    update file

--- Vending machine -list,method/30.HoangTienDung/Exam1/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-		static subintefacemachine sub = new subintefacemachine();
-		static machine machine1 = new machine();
-		public static void main(String[] args) {
-			machine1.machineProduct(sub);
-	}
-}
-				
