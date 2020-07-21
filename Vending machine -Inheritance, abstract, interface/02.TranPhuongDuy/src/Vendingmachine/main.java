9f01ff4bfaddffa4995a05537394034542f3d1ee
Cong <congnguyen1496@gmail.com>
2020-07-21 14:35:58 +0700

    asda

--- Vending machine -Inheritance, abstract, interface/02.TranPhuongDuy/src/Vendingmachine/main.java
+++
@@ -1,6 +0,0 @@
-package Vendingmachine;
-public public class main {
-	public static void main(String[]args) {
-		System.out.println("please insert coin");
-	}
-}
--- Vending machine -Inheritance, abstract, interface/12.VuThanhLong/project/src/VendingMachineInterface.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface VendingMachineInterface {
-	boolean isContinue(int money);
-	List<Product> chooseMachine(int choose);
-	void showMachine();
-	void showProduct(List<Product> listProduct);
-	int minPrice(List<Product> listProduct);
-}
