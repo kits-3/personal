5d2a026f77518b83c8e1f34e731db038ce577540
unknown <dung23cm@gmail.com>
2020-07-21 13:13:34 +0700

    update file vending machine I O

---
+++ Vending machine - Exception , thread , I O/Exam1/src/Main.java
@@ -0,0 +1,60 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+		static subintefacemachine subinterfacemachine = new subintefacemachine();
+		public static void main(String[] args) {
+			int Price =0;
+			
+			Scanner scan = new Scanner(System.in);
+			
+			boolean checkMore = true;
+		
+			System.out.println("product :");
+			
+			subinterfacemachine.addDrink();
+			
+			subinterfacemachine.getDrink();
+			do 
+			{
+				while(Price<subinterfacemachine.minDrink())
+				{
+					System.out.println("Your Price: "+Price);
+					System.out.println("Input money: ");
+					Price = scan.nextInt()+Price;
+				}
+				if(Price>=subinterfacemachine.minDrink())
+				{
+					subinterfacemachine.getDrink();
+					System.out.print("Input product :");
+					int b = scan.nextInt();
+					if(Price>=subinterfacemachine.listDrink.get(b-1).getPrice())
+					{						
+						Price = Price-subinterfacemachine.listDrink.get(b-1).getPrice();
+						System.out.println("Your had "+subinterfacemachine.listDrink.get(b-1).getPrice());
+						System.out.println("Your Price: "+Price);
+						System.out.println("Are you buy? (Y/N?)");
+						String c = scan.next();
+						if(c.equalsIgnoreCase("n"))
+						{
+							checkMore = false;
+						}
+						else if(c.equalsIgnoreCase("y"))
+						{
+							checkMore = true;
+						}
+						else
+						{
+							System.out.println("Input faild !");
+							checkMore = true;
+						}
+					}
+				}
+			}while(checkMore);
+			System.out.println("Your exchange: "+Price);
+			System.out.println("Thank you!\nGood bye !");
+			scan.close();
+	}
+}
+				
---
+++ Vending machine - Exception , thread , I O/Exam1/src/interfacemachine.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface interfacemachine {
+	
+	public List<Drink> addDrink();
+	public List<Foot> addFood();
+	public void getDrink();
+	public void getFood();
+	public int minDrink();
+	public int minFood();
+}
