7da75dc814d6fe888b4e58beb8a3e92ea3c0a80f
aspenmaude <huunghia.thnvtv@gmail.com>
2020-07-30 15:54:00 +0700

    delete mariaDB

--- MariaDB/No3.sql
+++
@@ -1,4 +0,0 @@
-SELECT name AS rank , (SELECT MAX(mount) FROM salary) hightestSalary 
-FROM rank WHERE id = (SELECT rank_id FROM user WHERE id = (SELECT user_id FROM salary WHERE mount = (SELECT MAX(mount) FROM salary)) )
-
-
